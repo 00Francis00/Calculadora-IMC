078f6332776d07ce89e701272e190f14054eaa19
Francisco Adriano <franciscoadrianoalmeida@gmail.com>
2024-12-20 19:26:26 -0300

    Update assets/script.js
    Co-authored-by: Lucas Ramos <81596809+lucasramosb@users.noreply.github.com>

--- assets/script.js
+++ assets/script.js
@@ -9,29 +9,22 @@ function calcular() {
     if (peso !=="" && altura !=="" && nome !==""){
 
         let valorIMC = (peso/altura**2).toFixed(1)
-        let msgRuim = `${nome}, seu índice IMC é considerado abaixo do normal.`
-        let msgNormal = `${nome}, seu índice IMC é considerado normal.`
-        let msgSobrePeso = `${nome}, Você está com sobrepeso.`
-        let msgObesidade_I = `.${nome}, Você está com obesidade de grau I.`
-        let msgObesidade_II = `${nome}, Você está com obesidade de grau II.`
-        let msgObesidade_III = `${nome}, Você está com obesidade de grau III.`
         let resultadoBox = document.querySelector('#resultadoBox')
 
-        
         resultadoBox.textContent = valorIMC
 
         if(valorIMC <= 18.5){
-            resultado.textContent = msgRuim
+            resultado.textContent = `${nome}, seu índice IMC é considerado abaixo do normal.`
         }else if (valorIMC >= 18.6 && valorIMC < 25.0) {
-            resultado.textContent = msgNormal
+            resultado.textContent = `${nome}, seu índice IMC é considerado normal.`
         }else if (valorIMC >= 25.0 && valorIMC < 30.0){
-            resultado.textContent = msgSobrePeso
+            resultado.textContent = `${nome}, Você está com sobrepeso.`
         }else if (valorIMC >= 30.0 && valorIMC < 35.0){
-            resultado.textContent = msgObesidade_I
+            resultado.textContent = `.${nome}, Você está com obesidade de grau I.`
         }else if (valorIMC >= 35.0 && valorIMC < 40.0){
-            resultado.textContent = msgObesidade_II
+            resultado.textContent = `${nome}, Você está com obesidade de grau II.`
         }else {
-            resultado.textContent = msgObesidade_III
+            resultado.textContent = `${nome}, Você está com obesidade de grau III.`
         }
 
     }else {
